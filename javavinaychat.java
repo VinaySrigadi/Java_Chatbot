a448c00cdba8c16be69d5c87a80f1e1aa21f419f
VinaySrigadi <vsrigadi@gmail.com>
2024-07-05 07:11:56 +0530

    First commit

---
+++ javavinaychat.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.Scanner.*;
+    public class javavinaychat{
+
+        public static void main(String[] args){  
+
+        Scanner sc = new Scanner(System.in);
+         
+        sc.close();
+
+         
+        }
+
+    }
